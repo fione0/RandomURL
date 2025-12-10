5c2d3bf544b94c7fe43ad018ad04cbd94b6fe650
fione0 <thatgirlfione@gmail.com>
2025-12-10 10:16:05 -0800

    Update urls.js

--- urls.js
+++ urls.js
@@ -1,5 +1,5 @@
 const urls = [
   "https://store.steampowered.com/app/3402150/Duck_Dating_Sim/",
-  "https://store.steampowered.com/app/1121910/I_Love_You_Colonel_Sanders_A_Finger_Lickin_Good_Dating_Simulator/"'
+  "https://store.steampowered.com/app/1121910/I_Love_You_Colonel_Sanders_A_Finger_Lickin_Good_Dating_Simulator/",
   "https://youtu.be/9J62hGda9BQ?si=5iGqxr3L0URC89KO"
 ];
