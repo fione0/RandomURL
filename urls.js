283a20b030e7f7151de551bd169ddd50045fea05
fione0 <thatgirlfione@gmail.com>
2025-12-10 10:43:11 -0800

    Update urls.js

--- urls.js
+++ urls.js
@@ -1,3 +1,9 @@
 const urls = [
-  "https://example.com"
+  "https://store.steampowered.com/app/1121910/I_Love_You_Colonel_Sanders_A_Finger_Lickin_Good_Dating_Simulator/",
+  "https://store.steampowered.com/app/3402150/Duck_Dating_Sim/",
+  "https://youtu.be/dQw4w9WgXcQ?si=WHgMWizRtb-DXpXs",
+  "https://youtu.be/9J62hGda9BQ?si=Fj7ykqV5DOW_qnnw",
+  "https://cheese.com",
+  "https://youtu.be/SXMh8WrXShw?si=VUuXnbfFilikTQ8b",
+  "https://store.steampowered.com/app/2923300/Banana/"
 ];
